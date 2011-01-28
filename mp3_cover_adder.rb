42d0ac17e0814d958b1b3f1102a51053a2cf3738
Patric Schielke <patric.schielke@gmail.com>
2011-01-28 21:06:24 +0100

    Improved feedback messages and small cleanup

--- mp3_cover_adder.rb
+++ mp3_cover_adder.rb
@@ -3,55 +3,60 @@
 require 'rubygems'
 require 'id3lib'
 
-ARGV.each do |a|
-  puts "Processing argument: #{a}"
+if ARGV.length > 0
+  ARGV.each do |a|
+    puts "Processing argument: #{a}"
 
-  if File.directory?(a)
-    # change to directory from command line argument
-    Dir.chdir(a)
-    puts "Now processing files in dir #{Dir.pwd}"
+    if File.directory?(a)
+      # change to directory from command line argument
+      Dir.chdir(a)
+      puts "Now processing files in dir #{Dir.pwd}"
 
-    # get all mp3 files in directory (and subdirs)
-    files = Dir.glob("*.mp3")
+      # get all mp3 files in directory (and subdirs)
+      files = Dir.glob("**/*.mp3")
 
-    if files.length > 0
-      files.each do |f|
-        puts " * Processing file: #{f}"
+      if files.length > 0
+        files.each do |f|
+          puts " * Processing file: #{f}"
 
-        # Load a tag from a file
-        tag = ID3Lib::Tag.new(f)
+          # Load a tag from a file
+          tag = ID3Lib::Tag.new(f)
 
-        # get music information 
-        title = tag.title
-        album = tag.album
+          # get music information 
+          title  = tag.title
+          album  = tag.album
+          artist = tag.artist
 
-        puts " ** Found album: '#{album}' and title: '#{title}'"
+          puts " ** tags: artist: '#{artist}', album: '#{album}', title: '#{title}'"
 
 =begin
-        # Get info about APIC frame to see which fields are allowed
-        ID3Lib::Info.frame(:APIC)
-        #=> [ 2, :APIC, "Attached picture",
-        #=>   [:textenc, :mimetype, :picturetype, :description, :data] ]
+          # Get info about APIC frame to see which fields are allowed
+          ID3Lib::Info.frame(:APIC)
+          #=> [ 2, :APIC, "Attached picture",
+          #=>   [:textenc, :mimetype, :picturetype, :description, :data] ]
 
-        # Add an attached picture frame
-        cover = {
-          :id          => :APIC,
-          :mimetype    => 'image/jpeg',
-          :picturetype => 3,
-          :description => 'Cover',
-          :textenc     => 0,
-          :data        => File.read('cover.jpg')
-        }
-        tag << cover
+          # Add an attached picture frame
+          cover = {
+            :id          => :APIC,
+            :mimetype    => 'image/jpeg',
+            :picturetype => 3,
+            :description => 'Cover',
+            :textenc     => 0,
+            :data        => File.read('cover.jpg')
+          }
+          tag << cover
 
-        # Last but not least, apply changes
-        tag.update!
+          # Last but not least, apply changes
+          tag.update!
 =end
+        end
+      else
+        puts " * No files in directory #{Dir.pwd} found for processing"
       end
     else
-      puts " * No files in directory #{Dir.pwd} found for processing"
+      puts "#{a} is not a directory"
     end
-  else
-    puts "#{a} is not a directory"
   end
+else
+  puts "missing arguments: please pass at least one directory path"
 end
