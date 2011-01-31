5d4ec76fde7cdc2c303330a02d68a56dd1ad6c26
Patric Schielke <patric.schielke@gmail.com>
2011-02-01 00:23:50 +0100

    Removed some unnecesary code. Skipped saving of temp file.

--- mp3_cover_adder.rb
+++ mp3_cover_adder.rb
@@ -18,98 +18,94 @@ if ARGV.length > 0
       # get all mp3 files in directory (and subdirs)
       files = Dir.glob("**/*.mp3")
 
-      if files.length > 0
-        files.each do |f|
-          puts " * Processing file: #{f}"
+      puts " * No files in directory #{Dir.pwd} found" if files.length < 1
 
-          # Load a tag from a file
-          tag = ID3Lib::Tag.new(f)
+      puts "   Found #{files.length} file(s) to process"
+      processed_files = 0
 
-          unless tag.nil?
-            # get music information 
-            title  = tag.title
-            album  = tag.album
-            artist = tag.artist
+      files.each do |f|
+        puts " * Processing file: #{f}"
 
-            puts " ** tags: artist: '#{artist}', album: '#{album}', title: '#{title}'"
+        # Load a tag from a file
+        tag = ID3Lib::Tag.new(f)
 
-            # check if album cover art already present
-            contains_cover = false
-            tag.entries.each do |e|
-              if e.has_key?(:id) and e[:id] == :APIC
-                contains_cover = true
-                break
-              end
+        unless tag.nil?
+          # get music information 
+          title  = tag.title
+          album  = tag.album
+          artist = tag.artist
+
+          puts " ** tags: artist: '#{artist}', album: '#{album}', title: '#{title}'"
+
+          # check if album cover art already present
+          contains_cover = false
+          tag.entries.each do |e|
+            if e.has_key?(:id) and e[:id] == :APIC
+              contains_cover = true
+              break
             end
+          end
 
-            unless contains_cover
-              # cover art missing in file
-              puts " *** Cover art missing, trying to get one"
+          unless contains_cover
+            # cover art missing in file
+            puts " *** Cover art missing, trying to get one"
 
-              # access Last.fm information
-              unless album.nil? || artist.nil?
-                begin
-                  album = Scrobbler::Album.new(artist, album, :include_info => true)
-                rescue
-                  album = Scrobbler::Album.new(artist, album)
+            # access Last.fm information
+            unless album.nil? || artist.nil?
+              begin
+                album = Scrobbler::Album.new(artist, album, :include_info => true)
+              rescue
+                album = Scrobbler::Album.new(artist, album)
+              end
+
+              cover_url  = album.image_large
+              
+              unless cover_url.nil?
+                puts "Cover image found at: #{cover_url}"
+
+                # tmp image could be created
+                puts " **** Inserting new cover into file"
+                
+                content_type = 'image/jpg'
+
+                open(cover_url) do |tmp_cover_file|
+                  content_type_src = tmp_cover_file.content_type
+                  content_type = content_type_src unless content_type_src.nil?
                 end
+                
+                image = open(cover_url).read
+                
+                cover = {
+                  :id          => :APIC,
+                  :mimetype    => content_type, 
+                  :picturetype => 3,
+                  :description => 'Cover',
+                  :textenc     => 0,
+                  :data        => image 
+                }
+                tag << cover
 
-                cover_url  = album.image_large
+                #save new cover
+                tag.update!
 
-                unless cover_url.nil?
-                  img_tmp_filename = 'image.tmp'
-                  File.open(img_tmp_filename, 'wb') do |file|
-                    file.print open(cover_url).read
-                  end
+                # update file processing counter
+                processed_files += 1
 
-                  unless f.nil? 
-                    # tmp image could be created
-                    puts " **** Inserting new cover into file"
-                    
-                    content_type = 'image/jpg'
-
-                    open(cover_url) do |tmp_cover_file|
-                      content_type_src = tmp_cover_file.content_type
-
-                      content_type = content_type_src unless content_type_src.nil?
-                    end
-                    
-                    cover = {
-                      :id          => :APIC,
-                      :mimetype    => content_type, 
-                      :picturetype => 3,
-                      :description => 'Cover',
-                      :textenc     => 0,
-                      :data        => File.read(img_tmp_filename)
-                    }
-                    tag << cover
-
-                    #save new cover
-                    tag.update!
-
-                    # new cover image saved
-                    puts " ***** New cover image stored in file"
-                    
-                    # delete temporary file
-                    File.delete(img_tmp_filename)
-                  else
-                    puts " **** New cover couldn't be downloaded"
-                  end
-                end
-              else
-                puts " **** Album information missing. Cannot use for Last.fm"
+                # new cover image saved
+                puts " ***** New cover image stored in file"
               end
             else
-              # cover already present
-              puts " *** Cover art already present"
+              puts " **** Album information missing. Cannot use for Last.fm"
             end
           else
-            puts " *** Tag could not be determined"
+            # cover already present
+            puts " *** Cover art already present"
           end
+        else
+          puts " *** Tag could not be determined"
         end
-      else
-        puts " * No files in directory #{Dir.pwd} found for processing"
       end
+        puts "   Number of mp3s tagged: #{processed_files}"
     else
       puts "#{a} is not a directory"
     end
